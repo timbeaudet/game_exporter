f74a996c8437d7138e8b0b4c9a661f6c78ef3be0
Timmy Sjöstedt <git@timsjo.se>
2018-04-07 14:30:19 +0200

    Basic export of objects

--- game_exporter.py
+++ game_exporter.py
@@ -1,4 +1,47 @@
+import bpy
+import json
+
+def serialize_vector3(vector):
+	return (vector[0], vector[1], vector[2])
+
+def serialize_matrix4(matrix):
+	return (
+		(matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3]),
+		(matrix[1][0], matrix[1][1], matrix[1][2], matrix[1][3]),
+		(matrix[2][0], matrix[2][1], matrix[2][2], matrix[2][3]),
+		(matrix[3][0], matrix[3][1], matrix[3][2], matrix[3][3]),
+	)
+
 def save(context,
 		filepath,
 		):
+
+	scene = context.scene
+	world = context.scene.world
+
+	objects = [obj for obj in scene.objects if obj.is_visible(scene)]
+
+	json_out = {
+		"objects": [],
+	}
+
+	for o in objects:
+		objdata = {
+			"name": o.name,
+			"type": o.type,
+			"transform": serialize_matrix4(o.matrix_world),
+			"position": serialize_vector3(o.matrix_world.to_translation()),
+		}
+
+		if "EMPTY" == o.type:
+			objdata.update({
+				"empty_type": o.empty_draw_type,
+				"empty_size": o.empty_draw_size,
+			})
+
+		json_out["objects"].append(objdata)
+
+	with open(filepath, "w", encoding="utf-8") as file:
+		file.write(json.dumps(json_out))
+
 	return {"FINISHED"}
