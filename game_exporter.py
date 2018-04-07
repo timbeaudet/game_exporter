9481ff34d85052a682fdfd1b4390d2660628b606
Timmy Sjöstedt <git@timsjo.se>
2018-04-07 15:44:41 +0200

    Basic curve export

--- game_exporter.py
+++ game_exporter.py
@@ -19,13 +19,15 @@ def save(context,
 	scene = context.scene
 	world = context.scene.world
 
-	objects = [obj for obj in scene.objects if obj.is_visible(scene) and "EMPTY" == obj.type]
+	objects_empty = [obj for obj in scene.objects if obj.is_visible(scene) and "EMPTY" == obj.type]
+	objects_curve = [obj for obj in scene.objects if obj.is_visible(scene) and "CURVE" == obj.type]
 
 	json_out = {
 		"objects": [],
+		"paths": [],
 	}
 
-	for o in objects:
+	for o in objects_empty:
 		objdata = {
 			"name": o.name,
 			"type": o.empty_draw_type,
@@ -36,6 +38,30 @@ def save(context,
 		}
 
 		json_out["objects"].append(objdata)
+
+	for o in objects_curve:
+		curve_type = o.data.splines.active.type
+
+		points = []
+		if "BEZIER" == curve_type:
+			for _, point in o.data.splines.active.bezier_points.items():
+				points.append({
+					"position": serialize_vector3(point.co),
+					"handle_left": serialize_vector3(point.handle_left),
+					"handle_right": serialize_vector3(point.handle_right),
+				})
+		else:
+			for _, point in o.data.splines.active.points.items():
+				points.append({"position": serialize_vector3(point.co)})
+
+		objdata = {
+			"name": o.name,
+			"type": o.data.splines.active.type,
+			"properties": {k:v for k,v in o.items()[1:]},
+			"points": points,
+		}
+
+		json_out["paths"].append(objdata)
 
 	with open(filepath, "w", encoding="utf-8") as file:
 		file.write(json.dumps(json_out, sort_keys=True, indent=4, separators=(',', ': ')))
